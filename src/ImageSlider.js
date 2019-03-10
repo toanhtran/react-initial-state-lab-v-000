87b14d50b30665dfd1ce8165c82af08940be0506
ToAnh Tran <toanh.t.tran@gmail.com>
2019-03-10 18:51:23 +0000

    Automatically backed up by Learn

--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,10 @@
 // your ImageSlider code here!
+import React from 'react';
+import ReactDom from 'react-dom';
+
+class ImageSlider extends React.Components {
+  
+}
+
+
+export default ImageSlider;
